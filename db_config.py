89affb1120a0ba62b74237acc121b9685c531de5
NancyJangra <jangranancy2005@gmail.com>
2026-04-17 23:33:29 +0530

    Remove sensitive file and add .gitignore

--- db_config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-DB_CONFIG = {
-    'host': os.environ.get('DB_HOST', 'localhost'),
-    'user': os.environ.get('DB_USER', 'root'),
-    'password': os.environ.get('DB_PASSWORD'),
-    'database': os.environ.get('DB_NAME', 'ecommerce_db')
-}
